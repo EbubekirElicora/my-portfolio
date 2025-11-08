82b099918d4d013bbf8c74edd5bcd1510b596d8b
EbubekirElicora <ebubekir.elicora@gmx.de>
2025-11-08 23:59:43 +0100

    new

--- src/app/components/legal/legal.component.ts
+++ src/app/components/legal/legal.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
-import { ScrollableSection } from '../../shared/scrollable-section.base';
-import { ScrollService } from '../../services/scroll.service';
 import { CommonModule } from '@angular/common';
 import { SideNavComponent } from '../side-nav/side-nav.component';
-
 @Component({
   selector: 'app-legal',
   standalone: true,
-  imports: [CommonModule,SideNavComponent],
+  imports: [CommonModule, SideNavComponent],
   templateUrl: './legal.component.html',
-  styleUrl: './legal.component.scss'
+  styleUrl: './legal.component.scss',
 })
-export class LegalComponent extends ScrollableSection {
-  override nextSectionId = 'About';
-  constructor(scroll: ScrollService) { super(scroll); }
+export class LegalComponent {
+  isRight = false;
+  goRight() {
+    this.isRight = true; // falls du es anderweitig noch brauchst
+    document
+      .getElementById('legal-right')
+      ?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
+  }
+
+  goLeft() {
+    this.isRight = false;
+    document
+      .getElementById('legal-left')
+      ?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
+  }
+
+  onKeyNext(ev: KeyboardEvent) {
+    const k = ev.key;
+    if (k === 'Enter' || k === ' ' || k === 'Spacebar') {
+      ev.preventDefault();
+      this.goRight();
+    }
+  }
+  onKeyBack(ev: KeyboardEvent) {
+    const k = ev.key;
+    if (k === 'Enter' || k === ' ' || k === 'Spacebar') {
+      ev.preventDefault();
+      this.goLeft();
+    }
+  }
 }
